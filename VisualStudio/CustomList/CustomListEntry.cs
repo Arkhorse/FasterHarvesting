9a2c099421249e4d1956a9d1e28d9291fba918a5
Arkhorse <theillusivemman@gmail.com>
2023-09-01 04:11:20 -0300

    Add documentation

--- VisualStudio/CustomList/CustomListEntry.cs
+++ VisualStudio/CustomList/CustomListEntry.cs
@@ -11,6 +11,11 @@ namespace FasterHarvesting.CustomList
         public virtual double ObjectBreakDownTime { get; set; }
 #nullable enable
 
+        /// <summary>
+        /// Creates a new instance of this class
+        /// </summary>
+        /// <param name="ObjectName">The exact name, case sensitive, of the prefab you want to target. Without the <c>.prefab</c> extension</param>
+        /// <param name="ObjectBreakDownTime">The amount of time, in hours, for how long the break down is done. (90 minutes is <c>1.5d</c>)</param>
         public ICustomListEntry(string ObjectName, double ObjectBreakDownTime)
         {
             this.ObjectName = ObjectName;
