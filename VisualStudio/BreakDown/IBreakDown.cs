7edc4351f5bafb4717ff968a99d5e34500438987
Arkhorse <theillusivemman@gmail.com>
2023-09-10 15:36:35 -0300

    Create IBreakDown.cs

---
+++ VisualStudio/BreakDown/IBreakDown.cs
@@ -0,0 +1,7 @@
+﻿namespace FasterHarvesting.BreakDown
+{
+    internal interface IBreakDown
+    {
+        public static void HarvestAlter(Panel_BreakDown breakDown, string NormalizeName) { }
+    }
+}
