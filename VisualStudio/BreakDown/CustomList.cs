d531207405fa7f28542018ffd897a57d59ee33ca
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-09-27 19:09:16 -0300

    Update CustomList.cs

--- VisualStudio/BreakDown/CustomList.cs
+++ VisualStudio/BreakDown/CustomList.cs
@@ -1,21 +1,23 @@
-﻿namespace FasterHarvesting.BreakDown
-{
-    internal class CustomList : IBreakDown
-    {
-        public static void HarvestAlter(Panel_BreakDown breakDown, string NormalizedName)
-        {
-            if (Settings.Instance.AllowCustomList && Main.entries.Count > 0)
-            {
-                // NormalizeName is needed here as (Clone) is extremely common
-                if (Main.entries.Exists(f => f.ObjectName == NormalizedName))
-                {
-                    breakDown.m_BreakDown.m_TimeCostHours = (float)Main.entries.Find(f => f.ObjectName == NormalizedName)!.ObjectBreakDownTime;
-                }
-            }
-            else if (Settings.Instance.AllowCustomList && Main.entries.Count == 0)
-            {
-                Logger.LogWarning("entries.Count is 0 while the setting is enabled");
-            }
-        }
-    }
-}
+﻿//namespace FasterHarvesting.BreakDown
+//{
+//    public class CustomList : IBreakDown
+//    {
+//		public static void HarvestAlter(Panel_BreakDown breakDown, string NormalizedName)
+//        {
+//            if (Settings.Instance.AllowCustomList && Main.entries.Count > 0)
+//            {
+//                Logging.Log("Custom List");
+//                // NormalizeName is needed here as (Clone) is extremely common
+//                if (Main.entries.Exists(f => f.ObjectName == NormalizedName))
+//                {
+//                    Logging.Log($"Object with name {NormalizedName} exists in the list");
+//                    breakDown.m_BreakDown.m_TimeCostHours = (float)Main.entries.Find(f => f.ObjectName == NormalizedName)!.ObjectBreakDownTime;
+//                }
+//            }
+//            else if (Settings.Instance.AllowCustomList && Main.entries.Count == 0)
+//            {
+//                Logging.LogWarning("entries.Count is 0 while the setting is enabled");
+//            }
+//        }
+//    }
+//}
