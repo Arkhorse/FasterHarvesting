637661fc0f31eb852f9106d93ca1015f4ef293aa
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-09-27 19:07:30 -0300

    Create Panel_BreakDown_BreakDownFinished.cs

---
+++ VisualStudio/Patches/Panel_BreakDown_BreakDownFinished.cs
@@ -0,0 +1,14 @@
+﻿namespace FasterHarvesting.Patches
+{
+    [HarmonyPatch(typeof(Panel_BreakDown), nameof(Panel_BreakDown.BreakDownFinished))]
+    public class Panel_BreakDown_BreakDownFinished
+    {
+        public static void Postfix()
+        {
+            if (Settings.Instance.GENERAL_PreserveCalories)
+            {
+                GameManager.GetHungerComponent().RemoveReserveCalories(Main.Config.BreakDownCalories);
+            }
+        }
+    }
+}
