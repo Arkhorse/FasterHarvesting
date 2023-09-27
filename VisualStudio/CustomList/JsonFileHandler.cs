57a462b13252300765f55d5e37822e48cbbdf6f6
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-09-27 19:08:58 -0300

    Create JsonFileHandler.cs

---
+++ VisualStudio/CustomList/JsonFileHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FasterHarvesting.CustomList
+{
+    public class JsonFileHandler
+    {
+        public class MainConfig
+        {
+            public static void Save()
+            {
+
+            }
+            public static void Load() { }
+        }
+
+    }
+}
