df80fc800bc5c2a75c433edd17ff7737d9e4cf12
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-09-27 19:07:27 -0300

    Create MainConfig.cs

---
+++ VisualStudio/MainConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FasterHarvesting
+{
+    /// <summary>
+    /// This contains the main config for the mod. Contains persistant stuff
+    /// </summary>
+    internal class MainConfig
+    {
+        public float BreakDownCalories { get; set; }
+    }
+}
