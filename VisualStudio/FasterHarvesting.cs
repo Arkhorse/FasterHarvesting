6a7eb717bc6071b9dc9b7c059a6d9dd4cbcccef1
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-09-27 19:09:26 -0300

    Update FasterHarvesting.cs

--- VisualStudio/FasterHarvesting.cs
+++ VisualStudio/FasterHarvesting.cs
@@ -1,22 +1,30 @@
 using FasterHarvesting.CustomList;
+using FasterHarvesting.Utilities;
 using MelonLoader.Utils;
+using System.Text.Json;
 
 namespace FasterHarvesting
 {
     public class Main : MelonMod
     {
-        internal static List<ICustomListEntry> entries = new();
-        internal static string CustomListFolder = Path.Combine(MelonEnvironment.ModsDirectory, "FasterHarvesting");
-        internal static string DemoFile = Path.Combine(CustomListFolder, "demo.json");
+        #region Constants
+        internal static string CustomListFolder { get; } = Path.Combine(MelonEnvironment.ModsDirectory, "FasterHarvesting");
+        internal static string DevelopementFolder { get; } = @"P:\Modding\The Long Dark\MyMods\FasterHarvesting\VisualStudio\Configs";
+        #endregion
+        #region CustomList
+        public static List<ICustomListEntry> ObjectsToAlter { get; set; } = new();
+        #endregion
+        #region Demo File
+        internal static string DemoConfigFile { get; } = @"demo.json";
+        internal static string DemoFile { get; } = Path.Combine(CustomListFolder, DemoConfigFile);
+        #endregion
+        #region Settings
         internal static bool LogInteractiveObjectDetails { get; set; }
-        internal static List<ICustomListEntry> DefaultObjects { get; } = new()
-        {
-            new ICustomListEntry("INTERACTIVE_LimbA_Prefab",        (float)Settings.Instance.limbATime),
-            new ICustomListEntry("INTERACTIVE_LimbB_Prefab",        (float)Settings.Instance.limbBTime),
-            new ICustomListEntry("INTERACTIVE_LimbA_Big_Prefab",    (float)Settings.Instance.limbABigTime),
-            new ICustomListEntry("INTERACTIVE_LimbB_Big_Prefab",    (float)Settings.Instance.limbBBigTime),
-            new ICustomListEntry("INTERACTIVE_BranchA_Prefab",      (float)Settings.Instance.branchATime)
-        };
+        #endregion
+        #region JsonFile
+        internal static JsonSerializerOptions options { get; } = new JsonSerializerOptions() { IncludeFields = true, WriteIndented = true };
+        internal static MainConfig Config { get; } = new MainConfig();
+        #endregion
 
         public override void OnInitializeMelon()
         {
@@ -24,12 +32,66 @@ namespace FasterHarvesting
             CustomListHandler.Initilize();
 
             uConsole.RegisterCommand("CreateDemoFile", new Action(CreateDemoFile));
+            uConsole.RegisterCommand("ReloadConfigFiles", new Action(ReloadConfigFiles));
+        }
+
+        public override void OnApplicationQuit()
+        {
+            base.OnApplicationQuit();
+
+            CopyConfigFilesToDev();
+        }
+
+        public static void CopyConfigFilesToDev()
+        {
+            if (Directory.Exists(DevelopementFolder) && Settings.Instance.CopyFilesToDev)
+            {
+                try
+                {
+                    string[] Files = Directory.GetFiles(CustomListFolder, "*.json");
+                    foreach (string file in Files)
+                    {
+                        if (File.Exists(Path.Combine(DevelopementFolder, file))) continue;
+                        File.Copy(file, Path.Combine(DevelopementFolder, file));
+                    }
+                }
+                catch
+                {
+                    Logging.LogWarning($"Copying files to dev failed");
+                    throw;
+                }
+            }
         }
 
         public static void CreateDemoFile()
         {
             CustomListHandler.SaveDemo();
             uConsole.Log($"Demo config file saved to {DemoFile}");
+        }
+
+        public static void ReloadConfigFiles()
+        {
+            CustomListHandler.ReInitilize();
+            uConsole.Log("Config files reloaded for Faster Harvesting");
+        }
+
+        public static void ForceClosePanel_BreakDown(Panel_BreakDown breakDown)
+        {
+            breakDown.Enable(false);
+        }
+
+        public static float GetBreakDownTimeMinutes(float time)
+        {
+            return time * 60;
+        }
+
+        public static bool ObjectExists(string name)
+        {
+            if (ObjectsToAlter.Exists(d => d.ObjectName == name))
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
