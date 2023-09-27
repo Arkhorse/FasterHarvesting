abc8116d92d06a25a7bbd7413238daf84f73422e
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-09-27 19:10:52 -0300

    remove BreakDown folder as included files are no longer required

--- VisualStudio/BreakDown/IBreakDown.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FasterHarvesting.BreakDown
-{
-    internal interface IBreakDown
-    {
-        public static void HarvestAlter(Panel_BreakDown breakDown, string NormalizeName) { }
-    }
-}
