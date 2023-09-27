5a9daf1e83a6960f45adb94d417cf2c53df2d431
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-09-27 19:08:11 -0300

    Update Settings.cs

--- VisualStudio/Settings/Settings.cs
+++ VisualStudio/Settings/Settings.cs
@@ -1,41 +1,19 @@
+using FasterHarvesting.CustomList;
+
 namespace FasterHarvesting
 {
     public class Settings : JsonModSettings
     {
         internal static Settings Instance { get; } = new();
 
+        /*[Name( "", Localize = true )]
+		[Description( "", Localize = true )]*/
+
         [Section("SETTING_SECTION_GENERAL", Localize = true)]
 
-        [Name("SETTING_GENERAL_REMEMBER_BREAKDOWN", Localize = true)]
-        public bool RememberBreakDown = false;
-
-        [Section("SETTING_SECTION_LIMB", Localize = true)]
-
-        [Name("GAMEPLAY_BranchBigSoft", Localize = true)]
-        [Slider(0.01f, 12.00f, NumberFormat = "{0:F2}")]
-        public float limbATime = 1.50f;
-
-        [Name("GAMEPLAY_BranchBigHard", Localize = true)]
-        [Slider(0.01f, 12.00f, NumberFormat = "{0:F2}")]
-        public float limbBTime = 1.50f;
-
-        [Name("GAMEPLAY_BranchBigSoft", Localize = true)]
-        [Slider(0.01f, 12.00f, NumberFormat = "{0:F2}")]
-        public float limbABigTime = 1.50f;
-
-        [Name("GAMEPLAY_BranchBigHard", Localize = true)]
-        [Slider(0.01f, 12.00f, NumberFormat = "{0:F2}")]
-        public float limbBBigTime = 1.50f;
-
-        [Name("GAMEPLAY_branch", Localize = true)]
-        [Slider(0.01f, 12.00f, NumberFormat = "{0:F2}")]
-        public float branchATime = 0.170f;
-
-        [Section("SETTING_SECTION_CUSTOM_LIST", Localize = true)]
-
-        [Name("SETTING_CUSTOM_LIST_ENABLE", Localize = true)]
-        [Description("SETTING_CUSTOM_LIST_ENABLE_DESC", Localize = true)]
-        public bool AllowCustomList = false;
+        [Name("SETTING_GENERAL_PRESERVE_CALORIES", Localize = true)]
+        [Description("SETTING_GENERAL_PRESERVE_CALORIES_DESC", Localize = true)]
+        public bool GENERAL_PreserveCalories = false;
 
         [Section("SETTING_SECTION_ADVANCED", Localize = true)]
 
@@ -43,17 +21,27 @@ namespace FasterHarvesting
         [Description("SETTING_ADVANCED_LOG_INTERACTIVE_DESC", Localize = true)]
         public bool InteractiveLog = false;
 
-        [Name("SETTING_ADVANCED_LOG_CUSTOM_LIST", Localize = true)]
-        public bool CustomListHandlerDebug = false;
+        [Name("SETTING_ADVANCED_LOG_LIST_ADD", Localize = true)]
+        public bool ADVANCED_listadd = false;
 
         [Name("SETTING_ADVANCED_LOG_DEFINITION", Localize = true)]
-        public bool LogDefinition = false;
+        public bool ADVANCED_LogUnused = false;
+
+        [Name("SETTING_ADVANCED_CREATECONFIG", Localize = true)]
+        [Description("SETTING_ADVANCED_CREATECONFIG_DESC", Localize = true)]
+        public bool ADVANCED_CreateNewConfig = false;
+
+        [Section("Dev Options")]
+
+        [Name("Copy Files to Dev folder")]
+        [Description("You should never use this as its only for dev environment")]
+        public bool CopyFilesToDev = false;
 
         protected override void OnConfirm()
         {
             base.OnConfirm();
 
-            Main.LogInteractiveObjectDetails = InteractiveLog;
+            Main.LogInteractiveObjectDetails = Instance.InteractiveLog;
         }
 
         // This is used to load the settings
