38e4be933dd2be7a4f1d08d7e1e7df20423d304a
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-10-01 21:54:48 -0300

    Create ConfigurationFile.cs

---
+++ VisualStudio/JsonFile/ConfigurationFile.cs
@@ -0,0 +1,12 @@
+﻿namespace FasterHarvesting.JsonFile
+{
+    public class ConfigurationFile
+    {
+        public string ObjectName { get; set; } = string.Empty;
+        public string ObjectTag { get; set; } = string.Empty;
+        public float ObjectBreadownTimeOriginal { get; set; }
+        public float ObjectBreakdownTimeModified { get; set; }
+        public List<string> ObjectBreakdownToolsOriginal { get; set; } = new();
+        public float ObjectBreakdownCaloriesOriginal { get; set; }
+    }
+}
