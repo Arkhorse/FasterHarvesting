2e8637341f2d51ec516d1a8fced479384654a58a
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-10-01 21:54:46 -0300

    Create ConfigurationHandler.cs

---
+++ VisualStudio/JsonFile/ConfigurationHandler.cs
@@ -0,0 +1,17 @@
+﻿using FasterHarvesting.Utilities;
+
+namespace FasterHarvesting.JsonFile
+{
+    public sealed class ConfigurationHandler
+    {
+        public static async void Load()
+        {
+
+        }
+
+        public static async void Save()
+        {
+
+        }
+    }
+}
